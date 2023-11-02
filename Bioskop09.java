cce3cdc000a977eef75453ddee2c74633d7515cd
ErikRidhoFirm <erikridh1@gmail.com>
2023-11-02 15:01:36 +0700

    menambahkan kode baru

--- Bioskop09.java
+++ Bioskop09.java
@@ -16,9 +16,15 @@ System.out.printf("%s \t %s\n", penonton[0][0], penonton [0][1]);
 System.out.printf("%s \t %s\n", penonton[1][0], penonton [1][1]);
 System.out.printf("%s \t %s\n", penonton[2][0], penonton [2][1]);
 System.out.printf("%s \t %s\n", penonton[3][0], penonton [3][1]);
+
+System.out.println(penonton.length);
+for (String[] barisPenonton : penonton) {
+    System.out.println("Panjang baris: " + barisPenonton.length);
+}
 System.out.println(penonton.length);
 for (int i = 0; i < penonton.length; i++){
 System.out.println("panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+
 System.out.println(penonton[0].length);
 System.out.println(penonton[1].length);
 System.out.println(penonton[2].length);
