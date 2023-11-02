d6195422ff5b86512d3fbfece1552974e1b7b50a
ErikRidhoFirm <erikridh1@gmail.com>
2023-11-02 14:33:25 +0700

    menambahkan kode program

---
+++ BioskopWithScanner09.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class BioskopWithScanner09{
+    public static void main(String []args){
+        Scanner input09 = new Scanner(System.in);
+       
+        int baris,kolom;
+        String nama, next;
+
+       String[][] penonton = new String[4][2];
+
+       while (true){
+            System.out.print("Masukkan nama: ");
+            nama = input09.nextLine();
+            System.out.print("Masukkan baris: ");
+            baris = input09.nextInt();
+            System.out.print("Masukkan kolom: ");
+            kolom = input09.nextInt();
+            input09.nextLine();
+
+            penonton[baris-1][kolom-1] = nama;
+
+            System.out.print("Input penonton lainnya? (y/n): ");
+            next = input09.nextLine();
+
+            if (next.equalsIgnoreCase("n")){
+                break;
+            }   
+        }
+    }
+}
---
+++ Numbers09.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Numbers09{
+    public static void main (String[]args){
+        Scanner input09 = new Scanner(System.in);
+        int [][] myNumbers = new int[3][];
+        myNumbers[0] = new int [5];
+        myNumbers[1] = new int [3];
+        myNumbers[2] = new int [1];
+    }
+}
